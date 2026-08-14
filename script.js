8533526bdc64bf3fb9ee8f98f0b978fb9b599dfa
Aman Yadav <ayadav771088@gmail.com>
2026-08-14 14:02:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const words = ["Aman YAdav ", "A Tech Enthusiast", "A Problem Solver", "An Engineer"];
+  let wordIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+  const typingElement = document.querySelector(".typing-text");
+
+  function typeEffect() {
+    const currentWord = words[wordIndex];
+    
+    if (isDeleting) {
+      typingElement.textContent = currentWord.substring(0, charIndex - 1);
+      charIndex--;
+    } else {
+      typingElement.textContent = currentWord.substring(0, charIndex + 1);
+      charIndex++;
+    }
+
+    let typeSpeed = isDeleting ? 60 : 120;
+
+    if (!isDeleting && charIndex === currentWord.length) {
+      typeSpeed = 2000; // Pura likhne ke baad 2 second rukega
+      isDeleting = true;
+    } else if (isDeleting && charIndex === 0) {
+      isDeleting = false;
+      wordIndex = (wordIndex + 1) % words.length;
+      typeSpeed = 400; // Agla word shuru karne se pehle chhota pause
+    }
+
+    setTimeout(typeEffect, typeSpeed);
+  }
+
+  typeEffect();
+});
