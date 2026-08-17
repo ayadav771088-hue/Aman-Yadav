9c90b7d246ffe8fb447ff65b017dcac4a4bbe224
Aman Yadav <ayadav771088@gmail.com>
2026-08-17 18:47:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const words = ["Aman YAdav ", "A Tech Enthusiast", "A Problem Solver", "An Engineer"];
+  const words = ["Aman Yadav ", "A Tech Enthusist", "A Problem Solver", "An Engineer"];
   let wordIndex = 0;
   let charIndex = 0;
   let isDeleting = false;
